dd809612169bef47da8848b50d2ccf83744ee2aa
Анастасия Базанова <nastya.is.fine@mail.ru>
2023-04-04 23:31:10 +0300

    initial commit

---
+++ src/main/java/NameMovie.java
@@ -0,0 +1,15 @@
+public class NameMovie {
+    private String movieName;
+
+    public NameMovie(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+}
---
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -0,0 +1,58 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+    @Test
+    public void addTest() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+
+        String[] actual = manager.findAll();
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void lastMoreThanLimitTest() {
+        MovieManager manager = new MovieManager(10);
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+        manager.addMovie("Film 8");
+        manager.addMovie("Film 9");
+        manager.addMovie("Film 10");
+        manager.addMovie("Film 11");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Film 11", "Film 10", "Film 9", "Film 8","Film 7","Film 6","Film 5","Film 4","Film 3","Film 2"};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void lastTest() {
+        MovieManager manager = new MovieManager(10);
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
