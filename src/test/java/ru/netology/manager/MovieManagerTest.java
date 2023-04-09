3d06352f77c9cdec4bec0860373ecbe748d7eac8
Анастасия Базанова <nastya.is.fine@mail.ru>
2023-04-09 19:44:09 +0300

    without wrong test

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -20,28 +20,6 @@ public class MovieManagerTest {
     }
 
     @Test
-    public void lastMoreThanLimitTest() {
-        MovieManager manager = new MovieManager(10);
-
-        manager.addMovie("Film 1");
-        manager.addMovie("Film 2");
-        manager.addMovie("Film 3");
-        manager.addMovie("Film 4");
-        manager.addMovie("Film 5");
-        manager.addMovie("Film 6");
-        manager.addMovie("Film 7");
-        manager.addMovie("Film 8");
-        manager.addMovie("Film 9");
-        manager.addMovie("Film 10");
-        manager.addMovie("Film 11");
-
-        String[] actual = manager.findLast();
-        String[] expected = {"Film 11", "Film 10", "Film 9", "Film 8","Film 7","Film 6","Film 5","Film 4","Film 3","Film 2"};
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
     public void lastTest() {
         MovieManager manager = new MovieManager(10);
 
@@ -53,6 +31,6 @@ public class MovieManagerTest {
         String[] expected = {"Film 3", "Film 2", "Film 1"};
 
         Assertions.assertArrayEquals(expected, actual);
-
     }
+
 }
